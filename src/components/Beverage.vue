b989f2be745b1673e64f074c156df93c4e81df1c
ArianaCo02 <144282253+ArianaCo02@users.noreply.github.com>
2025-11-02 23:02:55 -0500

    Update Beverage.vue

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -1,44 +1,60 @@
 <template>
-  <Mug>
-    <!-- show the mug type -->
-    <Cold v-if="isIced" />
-    <Hot v-else />
+  <div>
+    <div class="mug">
+ 
+    </div>
 
-    <Contents>
-      <!-- Creamer Layer -->
-      <template v-slot:top>
-        <Creamer v-if="creamer !== 'No Creamer'" :type="creamer" />
-      </template>
+    
+    <div>
+      <label v-for="temp in beverageStore.temperatures" :key="temp">
+        <input
+          type="radio"
+          v-model="beverageStore.selectedTemperature"
+          :value="temp"
+        />
+        {{ temp }}
+      </label>
+    </div>
 
-      <!-- Syrup Layer -->
-      <template v-slot:mid>
-        <Syrup v-if="syrup !== 'No Syrup'" :type="syrup" />
-      </template>
+    <!-- Base Options -->
+    <div>
+      <label v-for="b in beverageStore.bases" :key="b.id">
+        <input
+          type="radio"
+          v-model="beverageStore.selectedBase"
+          :value="b"
+        />
+        {{ b.name }}
+      </label>
+    </div>
 
-      <!-- Base Layer -->
-      <template v-slot:bottom>
-        <Base :type="base" />
-      </template>
-    </Contents>
-  </Mug>
+    <!-- Syrup Options -->
+    <div>
+      <label v-for="s in beverageStore.syrups" :key="s.id">
+        <input
+          type="radio"
+          v-model="beverageStore.selectedSyrup"
+          :value="s"
+        />
+        {{ s.name }}
+      </label>
+    </div>
+
+    <!-- Creamer Options -->
+    <div>
+      <label v-for="c in beverageStore.creamers" :key="c.id">
+        <input
+          type="radio"
+          v-model="beverageStore.selectedCreamer"
+          :value="c"
+        />
+        {{ c.name }}
+      </label>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import Contents from "./Contents.vue";
-import Mug from "./Mug.vue";
-import Syrup from "./Syrup.vue";
-import Base from "./Base.vue";
-import Creamer from "./Creamer.vue";
-import Hot from "./Hot.vue";
-import Cold from "./Cold.vue";
-
-// 🧠 Add new props here
-type Props = {
-  isIced: boolean;
-  creamer: string;
-  syrup: string;
-  base: string;
-};
-
-defineProps<Props>();
+import { useBeverageStore } from "../stores/beverageStore";
+const beverageStore = useBeverageStore();
 </script>
